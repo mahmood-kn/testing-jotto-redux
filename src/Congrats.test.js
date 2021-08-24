2d569755d62c869c1571d5f50656b5d6b47b1a58
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-08-24 14:17:53 +0430

    test proptypes

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,12 +1,13 @@
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Congrats from './Congrats';
-import { findByTestAttr } from './test/testUtils';
+import { findByTestAttr, checkProps } from './test/testUtils';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-
+const defaultProps = { success: false };
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test('renders without error', () => {
@@ -23,4 +24,9 @@ test('renders non-empty  when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: true };
+  checkProps(Congrats, expectedProps);
 });
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,3 +1,15 @@
+import checkPropsTypes from 'check-prop-types';
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
+};
+
+export const checkProps = (component, confirmingProps) => {
+  const propError = checkPropsTypes(
+    component.propTypes,
+    confirmingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
