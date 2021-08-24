510a1dffb968cd15ff47482ee5c377a35cf48068
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-08-24 15:27:58 +0430

    tests and code for non-zero guess

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -4,10 +4,30 @@ import PropTypes from 'prop-types';
 const GuessedWords = (props) => {
   return (
     <div data-test='component-guessed-words'>
-      {props.guessedWords.length === 0 && (
+      {props.guessedWords.length === 0 ? (
         <span data-test='guess-instructions'>
           Try to guess the secret word!
         </span>
+      ) : (
+        <div data-test='guessed-words'>
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matched Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.guessedWords.map((word, i) => (
+                <tr key={i} data-test='guessed-word'>
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
