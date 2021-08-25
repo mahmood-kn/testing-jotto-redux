b1fe52ba381c2c37b0cddf5f322b6414778ca914
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-08-25 11:34:43 +0430

    complet input component render test for sucess=false

--- src/Input.js
+++ src/Input.js
@@ -3,11 +3,30 @@ import { connect } from 'react-redux';
 
 export class Input extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div data-test='component-input'>
+        {this.props.success ? null : (
+          <form className='form-inline'>
+            <input
+              type='text'
+              data-test='input-box'
+              className='mb-2 mx-sm-3'
+              placeholder='Enter guess'
+            />
+            <button
+              data-test='submit-button'
+              className='btn btn-primary mb-2'
+              type='submit'>
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = ({ success }) => ({ success });
 
 const mapDispatchToProps = {};
 
